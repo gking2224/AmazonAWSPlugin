259e2361b00bceb6f14a6939bb79539eb93c2f98
Graham King <gking2224@gmail.com>
2016-11-02 15:20:46 +0000

    filter on running instances

--- src/main/groovy/me/gking2224/awsplugin/task/ec2/GetInstances.groovy
+++ src/main/groovy/me/gking2224/awsplugin/task/ec2/GetInstances.groovy
@@ -9,8 +9,15 @@ import com.amazonaws.services.ec2.model.Filter
 import com.amazonaws.services.ec2.model.Reservation
 
 
-
+/**
+ * http://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html
+ * 
+ * @author gk
+ *
+ */
 class GetInstances extends AbstractEC2Task {
+
+    static final RUNNING = "running" // per spec
 
     static final String CLIENT_TOKEN_FILTER = "client-token"
     static final String INSTANCE_IDS = "instance-ids"
@@ -35,6 +42,7 @@ class GetInstances extends AbstractEC2Task {
             filters << new Filter("tag:service", Collections.singletonList(service))
             filters << new Filter("tag:env", Collections.singletonList(env))
             filters << new Filter("tag:version", Collections.singletonList(version))
+            filters << new Filter("instance-state-code", RUNNING)
             rq.setFilters(filters)
             
             DescribeInstancesResult rs = getClient().describeInstances(rq)
